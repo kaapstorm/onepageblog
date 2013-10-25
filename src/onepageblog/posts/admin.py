c363a088f1c6f7517eadb6458afcf15a8137c660
Norman Hooper <norman.hooper@voss-solutions.com>
2013-10-25 12:03:36 +0200

    Added published_at timestamp

--- src/onepageblog/posts/admin.py
+++ src/onepageblog/posts/admin.py
@@ -1,40 +1,39 @@
 # This file is part of onepageblog.
-# 
+#
 # onepageblog is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # onepageblog is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with onepageblog.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.contrib import admin
 from onepageblog.posts.models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
     # List parameters
-    list_display = ('title', 'created_at', 'created_by', 'is_published')
+    list_display = ('title', 'created_by', 'is_published', 'published_at')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'created_by')
     date_hierarchy = 'created_at'
     search_fields = ('title', 'content_markdown')
-    
+
     # Detail parameters
-    fields = ('title', 'slug', 'content_markdown', 'is_published', 
-              'created_at', 'created_by')
+    fields = ('title', 'slug', 'content_markdown', 'created_by', 'created_at', 'is_published', 'published_at')
     prepopulated_fields = {'slug': ('title',)}
-    readonly_fields = ('created_at', 'created_by')
-    
+    readonly_fields = ('created_at', 'created_by', 'published_at')
+
     def save_model(self, request, obj, form, change):
         if not change:
             obj.created_by = request.user
         obj.save()
-    
+
 
 admin.site.register(Post, PostAdmin)
-
--- src/onepageblog/posts/models.py
+++ src/onepageblog/posts/models.py
@@ -1,21 +1,20 @@
 # This file is part of onepageblog.
-# 
+#
 # onepageblog is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # onepageblog is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with onepageblog.  If not, see <http://www.gnu.org/licenses/>.
-
-import markdown
+from datetime import datetime
 import re
-
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
@@ -23,6 +22,13 @@ from django.template.defaultfilters import slugify
 
 
 class Post(models.Model):
+    """
+    A blog post
+
+    `content_markdown` stores the content as entered by the blogger. `content`
+    stores safe HTML, which is generated from `content_markdown` by the `save`
+    method.
+    """
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     content_markdown = models.TextField(
@@ -32,16 +38,20 @@ class Post(models.Model):
     created_by = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
-    
+    published_at = models.DateTimeField()
+
     def __unicode__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('post_detail_view', kwargs={'slug': self.slug})
-    
+
     def save(self, *args, **kwargs):
-        """Converts markdown to safe HTML, and creates unique slug.
         """
+        Converts markdown to safe HTML, creates unique slug, and sets the
+        published_at timestamp.
+        """
+        # Convert Markdown to safe HTML
         # Useful: https://code.djangoproject.com/wiki/UsingMarkup
         # See also:
         # http://pypi.python.org/pypi/django-markupfield/
@@ -49,6 +59,8 @@ class Post(models.Model):
         # Enable extra features. Escape tags.
         self.content = markdown.markdown(self.content_markdown, ['extra'],
                                          safe_mode='escape')
+
+        # Create unique slug
         # Check slug is unique
         if self.slug is None or len(self.slug) == 0:
             # self.slug will be None if the post was created using PostForm
@@ -58,13 +70,17 @@ class Post(models.Model):
             self.slug = 'x'
         # Strip off any final digits
         match = re.match('(.+?)(?:-\d+)?$', self.slug)
-        initial_slug = match.groups()[0]
+        initial_slug = match.group(1)
         serial = 1
-        while Post.objects.exclude(id=self.id).filter(slug=self.slug)\
-                         .count() > 0:
+        while Post.objects.exclude(id=self.id).filter(slug=self.slug).count() > 0:
             serial += 1
             self.slug = '%s-%s' % (initial_slug, serial)
+
+        # Set published_at timestamp
+        if self.is_published and not self.published_at:
+            self.published_at = datetime.now()
+
         super(Post, self).save(*args, **kwargs)
-    
+
     class Meta:
         ordering = ['-created_at']
