b09630a8ceb50d9db4cb9f04a3cb57da49fc06b0
Norman Hooper <norman.hooper@voss-solutions.com>
2013-10-25 12:02:15 +0200

    Changed PostsFeed.items() to static method

--- src/onepageblog/posts/feeds.py
+++ src/onepageblog/posts/feeds.py
@@ -1,35 +1,35 @@
 # This file is part of onepageblog.
-# 
+#
 # onepageblog is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # onepageblog is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with onepageblog.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.contrib.syndication.views import Feed
 from onepageblog.posts.models import Post
 
+
 class PostsFeed(Feed):
     title = "Posts"
     link = "/"
-    
-    def items(self):
-        return Post.objects.filter(is_published=True)\
-                          .order_by('-created_at')[:10]
-    
+
+    @staticmethod
+    def items():
+        return Post.objects.filter(is_published=True).order_by('-created_at')[:10]
+
     def item_title(self, item):
         return item.title
-    
+
     def item_description(self, item):
         return item.content
-    
+
     def item_link(self, item):
         return item.get_absolute_url()
-    
