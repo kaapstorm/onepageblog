21a7c8acff53612d8497e3c479742a603a74590e
Norman Hooper <norman.hooper@voss-solutions.com>
2013-10-25 11:49:51 +0200

    Added skeleton for tests

--- src/onepageblog/posts/tests.py
+++ src/onepageblog/posts/tests.py
@@ -1,38 +1,48 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 # This file is part of onepageblog.
-# 
+#
 # onepageblog is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # onepageblog is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with onepageblog.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class TestPostModel(TestCase):
+    def test_save_creates_markdown(self):
+        # TODO: ...
+        pass
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_save_creates_unique_slug(self):
+        # TODO: ...
+        pass
 
+    def test_save_sets_published_at(self):
+        # TODO: ...
+        pass
+
+
+class TestViews(TestCase):
+    def test_post_list_view(self):
+        # TODO: ...
+        pass
+
+    def test_post_detail_view(self):
+        # TODO: ...
+        pass
+
+    def test_feed(self):
+        # TODO: ...
+        pass
+
+# TODO: Test user views
+
+# TODO: Test feed
