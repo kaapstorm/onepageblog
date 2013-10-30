200b3e3bf89ed0f1fcc5f11776dd935b40dcae85
Norman Hooper <norman@kaapstorm.com>
2013-10-31 00:00:43 +0200

    Skip posts with Unicode issues in Python 2

--- util/import_posts.py
+++ util/import_posts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import sys
 from django.contrib.auth.models import User
 import pymysql
@@ -37,60 +37,73 @@ norman = {
 
 
 def import_ttotd(record, user):
-    created_at = tz.localize(record[ttotd['fields'].index('created_at')])
-    post = Post(
-        title=record[ttotd['fields'].index('title')],
-        slug=record[ttotd['fields'].index('slug')],
-        content_markdown=record[ttotd['fields'].index('content_markdown')],
-        content=record[ttotd['fields'].index('content')],
-        created_by=user,
-        is_published=record[ttotd['fields'].index('is_published')],
-        published_at=created_at
-    )
-    post.save()
-    post.created_at = created_at  # Reset it from now() to original
-    post.save()
-    print('Saved {}'.format(post.slug))
+    fields = ttotd['fields']
+    print('Saving {}'.format(record[fields.index('slug')]))
+    created_at = tz.localize(record[fields.index('created_at')])
+    try:
+        post = Post(
+            title=unicode(record[fields.index('title')]),
+            slug=unicode(record[fields.index('slug')]),
+            content_markdown=unicode(record[fields.index('content_markdown')]).encode('utf-8'),
+            content=unicode(record[fields.index('content')]).encode('utf-8'),
+            created_by=user,
+            is_published=record[fields.index('is_published')],
+            published_at=created_at
+        )
+        post.save()
+    except UnicodeDecodeError:
+        print("\tFailed!")
+    else:
+        post.created_at = created_at  # Reset it from now() to original
+        post.save()
 
 
 def import_extrange(record, user):
-    created_at = tz.localize(record[extrange['fields'].index('created')])
-    post = Post(
-        title=record[extrange['fields'].index('title')],
-        slug=record[extrange['fields'].index('slug')],
-        summary=record[extrange['fields'].index('summary')],
-        content_markdown=record[extrange['fields'].index('body')],
-        content=record[extrange['fields'].index('body')],
-        created_by=user,
-        is_published=record[extrange['fields'].index('enabled')],
-        published_at=created_at
-        # summary is dropped
-        # body_type is ignored
-    )
-    post.save()
-    post.created_at = created_at
-    post.save()
-    print('Saved {}'.format(post.slug))
+    fields = extrange['fields']
+    print('Saving {}'.format(record[fields.index('slug')]))
+    created_at = tz.localize(record[fields.index('created')])
+    try:
+        post = Post(
+            title=unicode(record[fields.index('title')]),
+            slug=unicode(record[fields.index('slug')]),
+            summary=unicode(record[fields.index('summary')]),
+            content_markdown=unicode(record[fields.index('body')]),
+            content=unicode(record[fields.index('body')]),
+            created_by=user,
+            is_published=record[fields.index('enabled')],
+            published_at=created_at
+            # body_type is ignored
+        )
+        post.save()
+    except UnicodeDecodeError:
+        print("\tFailed!")
+    else:
+        post.created_at = created_at
+        post.save()
 
 
 def import_norman(record, user):
-    created_at = tz.localize(record[norman['fields'].index('created')])
-    post = Post(
-        title=record[norman['fields'].index('title')],
-        slug=record[norman['fields'].index('slug')],
-        summary=record[norman['fields'].index('summary')],
-        content_markdown=record[norman['fields'].index('body')],
-        content=record[norman['fields'].index('body')],
-        created_by=user,
-        is_published=record[norman['fields'].index('enabled')],
-        published_at=created_at
-        # summary is dropped
-        # image_tags is dropped
-    )
-    post.save()
-    post.created_at = created_at
-    post.save()
-    print('Saved {}'.format(post.slug))
+    fields = norman['fields']
+    print('Saving {}'.format(record[fields.index('slug')]))
+    created_at = tz.localize(record[fields.index('created')])
+    try:
+        post = Post(
+            title=unicode(record[fields.index('title')]),
+            slug=unicode(record[fields.index('slug')]),
+            summary=unicode(record[fields.index('summary')]),
+            content_markdown=unicode(record[fields.index('body')]),
+            content=unicode(record[fields.index('body')]),
+            created_by=user,
+            is_published=record[fields.index('enabled')],
+            published_at=created_at
+            # image_tags is dropped
+        )
+        post.save()
+    except UnicodeDecodeError:
+        print("\tFailed!")
+    else:
+        post.created_at = created_at
+        post.save()
 
 
 def get_username(conn, uid):
@@ -132,11 +145,11 @@ if __name__ == '__main__':
         import_post=import_ttotd,
         i_uid=ttotd['fields'].index('created_by_id')
     )
-    main(
-        settings=extrange,
-        import_post=import_extrange,
-        i_uid=extrange['fields'].index('author_id')
-    )
+    # main(
+    #     settings=extrange,
+    #     import_post=import_extrange,
+    #     i_uid=extrange['fields'].index('author_id')
+    # )
     main(
         settings=norman,
         import_post=import_norman
